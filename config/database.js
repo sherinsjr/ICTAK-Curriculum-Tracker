0334c7ed396984864c48c1e38adabb5835df2c79
ManjushaFSD <manjusha.r.nair93@gmail.com>
2023-05-08 17:57:21 +0530

    RequirementForm

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 const connectDatabase = ()=>{
     mongoose.connect("mongodb+srv://sherinrobert1952:K5qFcytSyx8tz7iB@cluster0.juoujt3.mongodb.net/ICTAKCurriculumDB?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true
           }).then((data)=>{
-            console.log(`Mogodb connected with the server: ${data.connection.host}`);
+            console.log(`MongoDB connected with the server: ${data.connection.host}`);
         });
 }
 
--- controllers/requirementFormContoller.js
+++ controllers/requirementFormContoller.js
@@ -1,6 +1,6 @@
 const Requirement = require("../models/requirmentFormModel");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 
 // Create Requirment --Admin
 exports.createRequirement = async (req, res) => {
@@ -35,77 +35,88 @@ exports.createRequirement = async (req, res) => {
     });
   }
 };
-// Get All Requirement
-exports.getAllRequirements = async(req,res)=>{
-  
-   try {
-    const requirements = await Requirement.find();
 
-    res.send(requirements).status(200);
-   } catch (error) {
-    res.status(400).json({
-        success:false,
-        message:error.message,
-    })
-   }
+// Get All Requirement
+exports.getAllRequirements = async(req,res,next)=>{
+  try {
+   const requirements = await Requirement.find();
+
+   res.send(requirements).status(200);
+  } catch (error) {
+   res.status(400).json({
+       success:false,
+       message:error.message,
+   })
+  }
 };
 
 // Get Requirement Details
-exports.getRequirementDetails = async (req, res) => {
-    try {
-      const requirement = await Requirement.findById(req.params.id);
-  
-      if (!requirement) {
-        return res.status(404).json({
-          success: false,
-          message: 'Requirement not found',
-        });
-      }
-  
-      res.status(200).json({
-        success: true,
-        data: requirement,
-      });
-    } catch (error) {
-      res.status(400).json({
-        success: false,
-        message: error.message,
-      });
-    }
-  };
-  
+exports.getRequirementDetails = async(req,res,next)=>{
+   try {
+       const requirement = await Requirement.findById(req.params.id);
 
-// Delete Requirement
+       res.status(200).json({
+           success:true,
+          requirement
+       })
+   } catch (error) {
+       res.status(400).json({
+           success:false,
+           message:error.message,
+       })
+   }
+}
+
 exports.deleteRequirement = async (req, res) => {
-   // Verify and decode the token
-   const token = req.headers.authorization.split(' ')[1];
-   const decodedToken = jwt.verify(token, "mysecretkey123"); 
-     // Check if the user is an admin
-     if (decodedToken.role !== "admin") {
-       return res.status(403).json({ message: "Access denied" });
-     }
-    try {
-      const id = req.params.id;
-      const deleteData = await Requirement.findOneAndDelete({ _id: id });
-  
-      if (!deleteData) {
-        return res.status(404).json({
-          success: false,
-          message: 'Requirement not found',
-        });
-      }
-  
-      res.status(200).json({
-        success: true,
-        message: 'Requirement deleted successfully...!',
-      });
-    } catch (error) {
-      res.status(400).json({
+  try {
+    const {id} = req.params
+    const deletedData = await Requirement.findOneAndDelete({ _id: id });
+
+    if (!deletedData) {
+      return res.status(404).json({
         success: false,
-        message: error.message,
+        message: "Requirement not found",
       });
     }
-  };
-  
-  
-  
+
+    res.status(200).json({
+      success: true,
+      message: "Requirement deleted successfully",
+      deletedData,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// Download Requirement File
+exports.downloadRequirementFile = async (req, res) => {
+  try {
+    const requirement = await Requirement.findById(req.params.id);
+    if (!requirement) {
+      return res.status(404).json({
+        success: false,
+        message: "Requirement not found",
+      });
+    }
+
+    const filePath = requirement.file;
+    if (!filePath) {
+      return res.status(404).json({
+        success: false,
+        message: "File not found",
+      });
+    }
+
+    const fileName = path.basename(filePath);
+    res.download(filePath, fileName);
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
